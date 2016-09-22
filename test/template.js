1d475d755497aa14c7cd934f69c71ab96bcdbc42
Jamie Smith <jamie.smith@blueacorn.com>
2016-09-22 07:49:59 -0400

    added template tests

---
+++ test/template.js
@@ -0,0 +1,24 @@
+var helpers = require('./helpers');
+var expect = require("chai").expect;
+var exec = require('child_process').exec;
+
+describe('CLI - Valid Templates', function() {
+
+    describe('Handlebar Templates', function() {
+        it("Valid template type", function(done) {
+            exec(helpers.appRoot + 'bin/cop --input examples/setting.json --template examples/views/Dockerfile.hbs',
+                function(error, stdout, stderr) {
+                    expect(stdout).to.equal(helpers.readFixture("Dockerfile01"));
+                    done();
+                });
+        });
+
+        it("Valid template type with multiple inputs", function(done) {
+            exec(helpers.appRoot + 'bin/cop --input examples/setting.json,examples/setting.yml --template examples/views/Dockerfile.hbs',
+                function(error, stdout, stderr) {
+                    expect(stdout).to.equal(helpers.readFixture("Dockerfile02"));
+                    done();
+                });
+        });
+    });
+});
