71ae23399d009a6d41ee9a2a221b9ed75d410180
Jamie Smith <jamie.smith@blueacorn.com>
2016-09-23 08:12:40 -0400

    added new args

--- test/template.js
+++ test/template.js
@@ -6,7 +6,7 @@ describe('CLI - Valid Templates', function() {
 
     describe('Handlebar Templates', function() {
         it("Valid template type", function(done) {
-            exec(helpers.appRoot + 'bin/cop --input examples/setting.json --template examples/views/Dockerfile.hbs',
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.hbs examples/setting.json',
                 function(error, stdout, stderr) {
                     expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile01"));
                     done();
@@ -14,7 +14,7 @@ describe('CLI - Valid Templates', function() {
         });
 
         it("Valid template type with multiple inputs", function(done) {
-            exec(helpers.appRoot + 'bin/cop --input examples/setting.json,examples/setting.yml --template examples/views/Dockerfile.hbs',
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.hbs examples/setting.json examples/setting.yml',
                 function(error, stdout, stderr) {
                     expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile02"));
                     done();
