ec1bdf07f7cdf93fa9e1537cfd20e535087d5ee7
Brice Burgess <nesta@iceburg.net>
2016-10-12 14:35:03 -0400

    add marko template engine

--- test/template.js
+++ test/template.js
@@ -57,4 +57,14 @@ describe('CLI - Valid Templates', function() {
                 });
         });
     });
+
+    describe('Marko Templates', function() {
+        it("render github push payload as html", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/github-push.marko test/fixtures/github-push.json',
+                function(error, stdout, stderr) {
+                    expect(stdout).to.equal(helpers.readFixture("github-push.html"));
+                    done();
+                });
+        });
+    });
 });
