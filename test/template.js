726e1994e98df36c1058166ddc8802624cf3f5bd
Jamie Smith <jamie.smith@blueacorn.com>
2016-10-14 13:21:46 -0400

    0.3.0 - Marko, Dust, and Nunjucks template engines

--- test/template.js
+++ test/template.js
@@ -21,4 +21,50 @@ describe('CLI - Valid Templates', function() {
                 });
         });
     });
+
+    describe('Dust Templates', function() {
+        it("Valid template type", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.dust examples/setting.json',
+                function(error, stdout, stderr) {
+                    expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile01"));
+                    done();
+                });
+        });
+
+        it("Valid template type with multiple inputs", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.dust examples/setting.json examples/setting.yml',
+                function(error, stdout, stderr) {
+                    expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile02"));
+                    done();
+                });
+        });
+    });
+
+    describe('Nunjucks Templates', function() {
+        it("Valid template type", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.njk examples/setting.json',
+                function(error, stdout, stderr) {
+                    expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile01"));
+                    done();
+                });
+        });
+
+        it("Valid template type with multiple inputs", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/Dockerfile.njk examples/setting.json examples/setting.yml',
+                function(error, stdout, stderr) {
+                    expect(stdout.trim()).to.equal(helpers.readFixture("Dockerfile02"));
+                    done();
+                });
+        });
+    });
+
+    describe('Marko Templates', function() {
+        it("render github push payload as html", function(done) {
+            exec(helpers.appRoot + 'bin/cop --render-template examples/views/github-push.marko test/fixtures/github-push.json',
+                function(error, stdout, stderr) {
+                    expect(stdout).to.equal(helpers.readFixture("github-push.html"));
+                    done();
+                });
+        });
+    });
 });
